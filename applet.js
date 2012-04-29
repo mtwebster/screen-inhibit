2ebac398de450f0c71a3ead97ee6ad0c5a267f88
Michael Webster <miketwebster@gmail.com>
2012-04-29 12:01:27 -0400

    V1.0

--- applet.js
+++ applet.js
@@ -15,9 +15,15 @@ const GLib = imports.gi.GLib;
 const Cinnamon = imports.gi.Cinnamon;
 
 
+const INHIBIT_TT = "Currently preventing screen-saver";
+const ALLOW_TT = "Currently allowing screen-saver";
+
+disp_state = 0; // inhibit off initially
+
 function MyApplet(orientation) {
     this._init(orientation);
 }
+
 
 MyApplet.prototype = {
     __proto__: Applet.IconApplet.prototype,
@@ -26,36 +32,48 @@ MyApplet.prototype = {
         Applet.TextIconApplet.prototype._init.call(this, orientation);
         
         try {                 
-           
-            this.set_applet_icon_name("web-browser");
+            this.set_applet_icon_symbolic_name('video-display-symbolic');
+            this.set_applet_tooltip(ALLOW_TT);
             this._orientation = orientation;
-         
-
         }
         catch (e) {
             global.logError(e);
         }
     },
-    
- 
-    
 
-    
     on_applet_clicked: function(event) {
-        this.menu.toggle();
-        global.stage.set_key_focus(this.searchEntry);
+        switch (disp_state) {
+            case 0:
+                this.set_applet_icon_symbolic_name('dialog-error-symbolic');
+                this.set_applet_tooltip(INHIBIT_TT);
+                disp_state = 1;
+                spawnCommandLineNoError("gnome-screensaver-command --exit");
+                spawnCommandLineNoError("xscreensaver-command -exit");
+                break;
+            case 1:
+                this.set_applet_icon_symbolic_name('video-display-symbolic');
+                this.set_applet_tooltip(ALLOW_TT);
+                disp_state = 0;
+                spawnCommandLineNoError("gnome-screensaver");
+                spawnCommandLineNoError("xscreensaver -nosplash");
+                break;
+        }
     },
     
-
     on_orientation_changed: function (orientation) {
         this._orientation = orientation;
         this._initContextMenu();
     }
-
-
-    
-    
 };
+
+
+function spawnCommandLineNoError(command_line) {
+    try {
+        let [success, argv] = GLib.shell_parse_argv(command_line);
+        Main.Util.trySpawn(argv);
+    } catch (err) {}
+}    
+
 
 function main(metadata, orientation) {  
     let myApplet = new MyApplet(orientation);
